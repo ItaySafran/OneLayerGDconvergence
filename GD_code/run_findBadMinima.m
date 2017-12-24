0719662d67ef69aa33aeec8e6c943e9aef9b861e
ItaySafran <34815176+ItaySafran@users.noreply.github.com>
2017-12-24 18:35:29 +0200

    Delete run_findBadMinima.m

--- GD_code/run_findBadMinima.m
+++
@@ -1,5 +0,0 @@
-function [Wbad,objVals] = run_findBadMinima(kV,kW,seed)
-V = [eye(kV)]; %%%%%%%%%%%%%%% ;zeros(kW,kV)];
-Tmax = 100; %%%%%%% 1000;
-rng(seed);
-[Wbad,objVals] = findBadMinima(kW,V,Tmax,false);
